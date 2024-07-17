08192a25d1ba5f661c4e636d6b3752ccfe23a267
yerm <leeyelim98@gmail.com>
2024-07-17 13:20:30 +0900

    기본 코드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Component1() {
+  return <div>1번 화면</div>
+}
+function Component2() {
+  return <div>2번 화면</div>
+}
+function Component3() {
+  return <div>3번 화면</div>
+}
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <h1>React Router DOM</h1>
+      <Component1 />
+      <Component2 />
+      <Component3 />
     </div>
   );
 }
